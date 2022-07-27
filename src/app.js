04018338b53969ea110198f3a52342686337a0e5
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 11:59:26 -0500

    second modules third commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
- const accountData =  fs.readFileSync('./json/accounts.json', 'utf8');
+ const accountData =  fs.readFileSync('src/json/accounts.json', 'utf8');
  const accounts = JSON.parse(accountData);
 
 
