8610187f5a7755b452e7a400a045914978cfd53e
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 11:15:24 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const fs  = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+app.set('view engine', 'ejs');
+
