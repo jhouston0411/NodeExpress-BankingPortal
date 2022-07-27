82caee8dbb6506f5800288fccb3ab6033d2e8a74
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 15:25:36 -0500

    76th commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+const accountData =  fs.readFileSync('src/json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData =  fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
