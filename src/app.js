3eafd5b272a8814645c3b2ec82bc36a6aef074e7
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 12:56:45 -0500

    5th commit

--- src/app.js
+++ src/app.js
@@ -22,6 +22,16 @@ app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary', accounts: accounts})
 })
 
+app.get('/savings', (req, res) =>{
+    res.render('account', {account: accounts.savings})
+})
+app.get('/checking', (req, res) =>{
+    res.render('account', {account: accounts.checking})
+})
+app.get('/credit', (req, res) =>{
+    res.render('account', {account: accounts.credit})
+})
+
 app.listen(3000, () =>{
     console.log("Running on port:", 3000)
 })
