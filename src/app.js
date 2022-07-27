7f5757e0cd9510ba087b2e25978b62042fed8926
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 13:42:42 -0500

    65th commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const fs  = require('fs');
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
