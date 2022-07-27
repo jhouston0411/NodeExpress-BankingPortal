412a532fb0e58d8d2bb80af2f5ca5003d1cde39f
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 11:50:56 -0500

    second modules first commit

--- src/app.js
+++ src/app.js
@@ -4,10 +4,20 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const accounts = " ";
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
+
+fs.readFile(path.join(__dirname, 'accounts.json'), 'utf8', (err, accountData) => {
+    if(err){
+        console.log(err)
+    }else{
+        accounts = JSON.parse(accountData);
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'index'})
