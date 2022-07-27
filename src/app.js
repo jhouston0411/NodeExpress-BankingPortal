3bd648c0e4bb6c0a4bdfe918385350f40bb236b7
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 11:54:27 -0500

    second modules secondt commit

--- src/app.js
+++ src/app.js
@@ -4,19 +4,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const accounts = " ";
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
-fs.readFile(path.join(__dirname, 'accounts.json'), 'utf8', (err, accountData) => {
-    if(err){
-        console.log(err)
-    }else{
-        accounts = JSON.parse(accountData);
-    }
-})
+ const accountData =  fs.readFileSync(path.join(__dirname, 'accounts.json'), 'utf8');
+ const accounts = JSON.parse(accountData);
+
 
 
 app.get('/', (req, res) => {
