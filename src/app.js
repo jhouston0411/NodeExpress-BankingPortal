ce90c63e999ffd32f6d54a68595c8e8e8a7c5ee4
jhouston0411 <john.houston0411@hotmail.com>
2022-07-27 11:16:15 -0500

    second commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
